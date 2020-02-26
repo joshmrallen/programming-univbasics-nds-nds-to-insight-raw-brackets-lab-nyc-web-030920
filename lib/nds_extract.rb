7c77d9a98841108b25f86ba5682ee936c9950534
delightful-source-3050 <joshmrallen@gmail.com>
2020-02-26 04:12:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,17 +6,24 @@ def directors_totals(nds)
   # that you know what you're starting with! remember it's an AofHofAofH
   pp nds
   
-  #director names can be found at: nds[main_array_index][:name]
-  #worldwide_gross can be found at: nds[main_array_index][:movies][movie_index][:worldwide_gross]
-  #the result will be an HofH
-  
-  
-  
-  
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+    # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
     
   }
+  
+  #director names can be found at: nds[main_array_index][:name]
+  #worldwide_gross can be found at: nds[main_array_index][:movies][movie_index][:worldwide_gross]
+  #the result will be a simple hash
+  
+  main_array_index = 0
+  while main_array_index < nds.count do
+    result.push(nds[main_array_index][:name])
+  end
+  
+  
+  
+  
+
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
